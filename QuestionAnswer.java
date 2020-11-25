ad2cb80f6797967b560aaa32a5b004e682164c34
Akshit Arora <aeora44@uw.edu>
2020-11-24 19:54:34 -0800

    Adding test file for QA

--- QuestionAnswer.java
+++ QuestionAnswer.java
@@ -1,13 +1,9 @@
+package Core;
+
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
-import java.util.*;
-import java.io.*;
-
 
 public class QuestionAnswer {
-	
 	/**
 	 * 
 	 */
@@ -25,16 +21,36 @@ public class QuestionAnswer {
 	
 	public QuestionAnswer(final int rows, final int numberDoors) {
 		questionGrid = new String[rows][numberDoors];
-      questionMap = new HashMap<>();
-      hintsMap = new HashMap<>();
+		setQuestionMap(new HashMap<>());
+		setHintsMap(new HashMap<>());
 	}
    
-   public String getDimensions() {
-      return "" + questionGrid.length + ", " + questionGrid[0].length; 
-   }
+	public String getDimensions() {
+		return "" + questionGrid.length + ", " + questionGrid[0].length; 
+	}
    
-   public static void main(final String[][] theArgs) {
-      QuestionAnswer qa = new QuestionAnswer(3, 5);
-      System.out.println(qa.getDimensions());
-   }
+
+	public Map<String, String> getQuestionMap() {
+		return questionMap;
+	}
+
+	public void setQuestionMap(Map<String, String> questionMap) {
+		this.questionMap = questionMap;
+	}
+	
+	public Map<String, String> getHintsMap() {
+		return hintsMap;
+	}
+
+	public void setHintsMap(Map<String, String> hintsMap) {
+		this.hintsMap = hintsMap;
+	}
+
+   
+	public static void main(final String[][] theArgs) {
+		QuestionAnswer qa = new QuestionAnswer(3, 5);
+	    System.out.println(qa.getDimensions());
+	}
+
 }
+
---
+++ QuestionAnswerTest.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package Core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author stlp
+ *
+ */
+public class QuestionAnswerTest {
+
+	/**
+	 * Test method for {@link Core.QuestionAnswer#QuestionAnswer(int, int)}.
+	 */
+	@Test
+	public void testQuestionAnswer() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Core.QuestionAnswer#getDimensions()}.
+	 */
+	@Test
+	public void testGetDimensions() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Core.QuestionAnswer#getQuestionMap()}.
+	 */
+	@Test
+	public void testGetQuestionMap() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Core.QuestionAnswer#setQuestionMap(java.util.Map)}.
+	 */
+	@Test
+	public void testSetQuestionMap() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Core.QuestionAnswer#getHintsMap()}.
+	 */
+	@Test
+	public void testGetHintsMap() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Core.QuestionAnswer#setHintsMap(java.util.Map)}.
+	 */
+	@Test
+	public void testSetHintsMap() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Core.QuestionAnswer#main(java.lang.String[][])}.
+	 */
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
