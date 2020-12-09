d2d6688b9d991b0c0d7c8a49972d18c6454730b3
Alex <37315062+PoplarBeach@users.noreply.github.com>
2020-12-08 18:36:01 -0800

    Update TriviaMaze.java
    Added functionality in the way of opening the GUI and starting the game.

--- TriviaMaze.java
+++ TriviaMaze.java
@@ -1,1 +1,20 @@
+import java.awt.EventQueue;
 
+/*Main for the MazeRunner game. 
+ * Finished? */
+public class TriviaMaze {
+	/**
+	 * 
+	 * @param theArgs
+	 */
+	public static void main(String[] theArgs) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                GuiMain frame = new GuiMain();
+            	frame.start();
+            }
+        });
+	}
+
+}
